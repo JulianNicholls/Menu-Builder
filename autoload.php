c327023b2f0e016c3224b8fd9136659b5a0fa960
Julian Nicholls <juliannicholls29@gmail.com>
2014-06-24 18:07:02 +0100

    Added #bootstrapItems.

--- autoload.php
+++ autoload.php
@@ -3,3 +3,22 @@ function __autoload( $class )
 {
   require_once( strtolower( $class ) . '.php' );
 }
+
+function bootstrapItems( $items )
+{
+  if( !is_array( $items ) )
+    $items = $items->roots();
+    
+  foreach( $items as $item ) : ?>
+    <li <?php if( $item->hasChildren() ) echo 'class="dropdown"'; ?>>
+      <a href="<?php echo $item->link->get_url() ?>" <?php if( $item->hasCildren() ) echo 'class="dropdown-toggle" data-toggle="dropdown"'; ?>>
+        <?php echo $item->link->get_text() ?> <?php if( $item->hasChildren() ) echo '<b class="caret"></b>'?>
+      </a>
+      <?php if( $item->hasChildren() ) : ?>
+        <ul class="dropdown-menu">
+          <?php bootstrapItems( $item->children() ); ?>
+        </ul>
+      <?php endif; ?>
+    </li>
+  <?php endforeach;
+}
